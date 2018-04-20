78486a83536e2dfce6c5b8739840fbc2ebd62de8
Rui Duarte <r3v3nant@gmail.com>
2018-04-20 18:01:04 +0100

    diagramas+db(2)
    -Actualização diagramas e DB de forma a reflectir últimas alterações;
    -Tabela de roles e expressões populada;

---
+++ SQL Scripts/inserts.sql
@@ -0,0 +1,19 @@
+﻿--Users
+INSERT INTO role VALUES 
+	(1, 'Admin'), 
+	(2, 'Prof Saúde'), 
+	(3, 'Utente');
+
+--Expressoes
+INSERT INTO expressoes VALUES
+	(1, 'Queimadura'),
+    (2, 'Sensação de frio doloroso'),
+    (3, 'Choques Eléctricos'),
+    (4, 'Formigueira'),
+    (5, 'Picadas'),
+    (6, 'Dormência'),
+    (7, 'Comichão'),
+    (8, 'Hipoestesia ao tacto'),
+    (9, 'Hipoestesia à picada'),
+    (10, 'Fricção leve ("brushing")');
+    
--- SQL Scripts/ptweb.sql
+++ SQL Scripts/ptweb.sql
@@ -15,7 +15,7 @@ CREATE TABLE logs (
   PRIMARY KEY (id));
 CREATE TABLE role (
   id   SERIAL NOT NULL, 
-  nome varchar(20) NOT NULL, 
+  nome varchar(50) NOT NULL, 
   PRIMARY KEY (id));
 CREATE TABLE artigo (
   id           SERIAL NOT NULL, 
@@ -26,35 +26,30 @@ CREATE TABLE artigo (
   data_edicao  timestamp, 
   PRIMARY KEY (id));
 CREATE TABLE categoria (
-  id SERIAL NOT NULL, 
+  id           SERIAL NOT NULL, 
+  id_expressao int4 NOT NULL, 
+  id_zona      int4 NOT NULL, 
   PRIMARY KEY (id));
 CREATE TABLE artigo_categoria (
-  artigoid    int4 NOT NULL, 
-  categoriaid int4 NOT NULL, 
-  PRIMARY KEY (artigoid, 
-  categoriaid));
+  artigo_id    int4 NOT NULL, 
+  categoria_id int4 NOT NULL, 
+  PRIMARY KEY (artigo_id, 
+  categoria_id));
 CREATE TABLE episodio_dor (
   id          SERIAL NOT NULL, 
   data        date NOT NULL, 
   zona        int4 NOT NULL, 
   intensidade int4 NOT NULL, 
-  tipo        int4 NOT NULL, 
   utente      int4 NOT NULL, 
   concluido   bool NOT NULL, 
   PRIMARY KEY (id));
-CREATE TABLE tipo (
-  id   int4 NOT NULL, 
+CREATE TABLE expressoes (
+  id   SERIAL NOT NULL, 
   nome varchar(50) NOT NULL, 
   PRIMARY KEY (id));
-CREATE TABLE intensidade (
-  id        int4 NOT NULL, 
-  valor     int4 NOT NULL, 
-  descricao varchar(255), 
-  PRIMARY KEY (id));
 CREATE TABLE zona (
-  id        int4 NOT NULL, 
-  nome      varchar(50) NOT NULL, 
-  descricao varchar(255), 
+  id   SERIAL NOT NULL, 
+  nome varchar(50) NOT NULL, 
   PRIMARY KEY (id));
 CREATE TABLE mensagem (
   id       int4 NOT NULL, 
@@ -109,20 +104,21 @@ CREATE TABLE Video_categoria (
   categoriaid int4 NOT NULL, 
   PRIMARY KEY (Videoid, 
   categoriaid));
+CREATE TABLE expressoes_episodio_dor (
+  expressoes_id   int4 NOT NULL, 
+  episodio_dor_id int4 NOT NULL, 
+  estado          bool NOT NULL, 
+  PRIMARY KEY (expressoes_id, 
+  episodio_dor_id));
 ALTER TABLE utente ADD CONSTRAINT FKutente662518 FOREIGN KEY (id) REFERENCES utilizador (id);
 ALTER TABLE utilizador ADD CONSTRAINT FKutilizador782488 FOREIGN KEY (role) REFERENCES role (id);
-ALTER TABLE artigo_categoria ADD CONSTRAINT FKartigo_cat204593 FOREIGN KEY (artigoid) REFERENCES artigo (id);
-ALTER TABLE artigo_categoria ADD CONSTRAINT FKartigo_cat832702 FOREIGN KEY (categoriaid) REFERENCES categoria (id);
+ALTER TABLE artigo_categoria ADD CONSTRAINT FKartigo_cat840657 FOREIGN KEY (artigo_id) REFERENCES artigo (id);
+ALTER TABLE artigo_categoria ADD CONSTRAINT FKartigo_cat638093 FOREIGN KEY (categoria_id) REFERENCES categoria (id);
 ALTER TABLE episodio_dor ADD CONSTRAINT FKepisodio_d207728 FOREIGN KEY (utente) REFERENCES utente (id);
-ALTER TABLE episodio_dor ADD CONSTRAINT FKepisodio_d549683 FOREIGN KEY (tipo) REFERENCES tipo (id);
-ALTER TABLE episodio_dor ADD CONSTRAINT FKepisodio_d630886 FOREIGN KEY (intensidade) REFERENCES intensidade (id);
 ALTER TABLE episodio_dor ADD CONSTRAINT FKepisodio_d918555 FOREIGN KEY (zona) REFERENCES zona (id);
 ALTER TABLE artigo ADD CONSTRAINT FKartigo676544 FOREIGN KEY (autor) REFERENCES Profissional_Saude (id);
 ALTER TABLE treino ADD CONSTRAINT FKtreino913584 FOREIGN KEY (prof_saude) REFERENCES Profissional_Saude (id);
 ALTER TABLE treino ADD CONSTRAINT FKtreino161551 FOREIGN KEY (utente) REFERENCES utente (id);
-ALTER TABLE zona ADD CONSTRAINT FKzona596722 FOREIGN KEY (id) REFERENCES categoria (id);
-ALTER TABLE intensidade ADD CONSTRAINT FKintensidad322001 FOREIGN KEY (id) REFERENCES categoria (id);
-ALTER TABLE tipo ADD CONSTRAINT FKtipo412286 FOREIGN KEY (id) REFERENCES categoria (id);
 ALTER TABLE Profissional_Saude ADD CONSTRAINT FKProfission960750 FOREIGN KEY (id) REFERENCES utilizador (id);
 ALTER TABLE mensagem ADD CONSTRAINT FKmensagem76278 FOREIGN KEY (destino) REFERENCES utilizador (id);
 ALTER TABLE mensagem ADD CONSTRAINT FKmensagem908651 FOREIGN KEY (origem) REFERENCES utilizador (id);
@@ -132,3 +128,7 @@ ALTER TABLE consultas ADD CONSTRAINT FKconsultas238912 FOREIGN KEY (utente) REFE
 ALTER TABLE Video ADD CONSTRAINT FKVideo73440 FOREIGN KEY (autor) REFERENCES Profissional_Saude (id);
 ALTER TABLE Video_categoria ADD CONSTRAINT FKVideo_cate227648 FOREIGN KEY (Videoid) REFERENCES Video (id);
 ALTER TABLE Video_categoria ADD CONSTRAINT FKVideo_cate290999 FOREIGN KEY (categoriaid) REFERENCES categoria (id);
+ALTER TABLE expressoes_episodio_dor ADD CONSTRAINT FKexpressoes77142 FOREIGN KEY (expressoes_id) REFERENCES expressoes (id);
+ALTER TABLE categoria ADD CONSTRAINT FKcategoria376302 FOREIGN KEY (id_zona) REFERENCES zona (id);
+ALTER TABLE categoria ADD CONSTRAINT FKcategoria32065 FOREIGN KEY (id_expressao) REFERENCES expressoes (id);
+ALTER TABLE expressoes_episodio_dor ADD CONSTRAINT FKexpressoes905691 FOREIGN KEY (episodio_dor_id) REFERENCES episodio_dor (id);
