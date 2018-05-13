3952a97b3c3355917edd3e3f0d08d5ed48cb6ec3
JoaoNovo <joao.novo.teixeira@gmail.com>
2018-05-13 19:28:07 +0100

    "Added pasta videos e added class_artigos."

---
+++ public/includes/php/class_artigos.php
@@ -0,0 +1,29 @@
+<?php
+
+class artigos
+{
+
+    private $lista = [ 
+                    "artigo" => [
+                        "autor" => "",
+                        "titulo" => "",
+                        "conteudo" => "",
+                        "categorias" => [
+                            "expressao" => [],
+                            "zonas" => []
+                        ]
+                    ]];
+
+    public function listaArtigos()
+    {
+        $GLOBALS[""];
+        
+
+    
+        return $lista;
+    }
+
+}
+
+
+?>
