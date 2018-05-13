e8828e95a170d7871d6b6571951513920de45c7f
luispinho <lf.pinho27@gmail.com>
2018-05-13 18:45:42 +0100

    Dados de conexão ao server atualizados.

--- public/includes/php/class_database.php
+++ public/includes/php/class_database.php
@@ -14,10 +14,10 @@ class Database extends PDO
 
     public function Database()
     {
-        $this->database = "grp4-412";
-        $this->hostname = "estga-prog.ua.pt";
-        $this->username = "ptw";
-        $this->password = "ptw";
+        $this->database = "ptaw-gr4-2018";
+        $this->hostname = "estga-dev.clients.ua.pt";
+        $this->username = "ptaw-gr4-2018";
+        $this->password = "8%23wK988Z";
         $this->port = "5432";
 
         $this->db = new PDO("pgsql:host=$this->hostname;dbname=$this->database;port=$this->port;charset=utf8", $this->username, $this->password);
