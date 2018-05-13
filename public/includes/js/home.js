39e8c0cb23a4f3b573d542fae749c35241111959
JoaoNovo <joao.novo.teixeira@gmail.com>
2018-05-13 18:58:31 +0100

    Home - admin.php Alterado

---
+++ public/includes/js/home.js
@@ -0,0 +1,10 @@
+/* ADMIN */
+
+
+$(document).ready(function(){
+
+    
+    $("#openDropDown").click(function(){
+        $(".dropdown-container").slideToggle( "slow" );
+    });
+});
--- public/modules/home/home_admin_navbar.php
+++ public/modules/home/home_admin_navbar.php
@@ -1,6 +1,6 @@
 <div class="row">
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#"><i class="fas fa-home"></i></a>
+        <a class="navbar-brand" href="#">AdminPanel</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
