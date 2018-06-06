8fb2dbb3940049eb8e2e33c821d576bedd3df208
Rui Duarte <r3v3nant@gmail.com>
2018-06-06 22:46:14 +0100

    pintar (highlight)
    -Implementação do highlight da zona seleccionada

--- pintar/js/pintar.js
+++ pintar/js/pintar.js
@@ -1,7 +1,7 @@
 //Custom hot to cold temperature color gradient
 //http://web-tech.ga-usa.com/2012/05/creating-a-custom-hot-to-cold-temperature-color-gradient-for-use-with-rrdtool/index.html
 
-var zona, cor, valor;
+var zona, cor, valor, corHex;
 var corArray = [
     "#FF0000",
     "#FF6e00",
@@ -21,19 +21,12 @@ $(document).ready(function () {
     $("#pickedColor").css("background-color", corArray[0]);
     $("#valor").val(defaultValor);
 
-    $('.map').maphilight({fade: false});
-
-    $(".grid-item").click(function () {
-        pintarZonaCorpo($(this));
-        cleanURL($(this));
-        alert(zona);
-    a.toU
-    });
     createSlider();
-    setCores();
+    createGradient();
+    highlight();
 });
 
-function setCores() {
+function createGradient() {
     var gradient = "linear-gradient(to right";
     for (var i=0; i<corArray.length ; i++) gradient += ", " + corArray[i];
     gradient += ")";
@@ -58,20 +51,35 @@ function createSlider () {
     });
 }
 
-
-function pintarZonaCorpo(e) {
-    zona = e.css("background-Image");
-    if (zona == "none" || zona == "") {
-        e.css({
-            "background-color": $("#pickedColor").css("backgroundColor")
-        });
+function rgbToHex(rgb) {
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+        return ("0" + parseInt(x).toString(16)).slice(-2);
     }
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
 
-function cleanURL (e) {
-    //cleanup do url p obter apenas o nome do ficheiro sem extensão
-    var url = e.css("background-Image");
-    var cleanup = /\"|\'|\)/g;
-    var filename = url.split('/').pop().replace(cleanup, '');
-    zona = filename.substring(0,filename.length-4);
+function createMap () {
+    corHex = rgbToHex(cor).slice(1);
+    console.log("corHex",corHex);
+    console.log("zona",zona);
+    console.log("valr",valor);
+    $('.map').maphilight({
+        fillColor: corHex,
+        strokeColor: corHex,
+        strokeWidth: 5,
+        fillColor:corHex,
+        fillOpacity:0.6
+    });
+}
+
+function highlight(e) {
+    $('.corpo').click(function(e) {
+        zona = this.alt;;
+        createMap();
+        e.preventDefault();
+        var data = $('.corpo').mouseout().data('maphilight') || {};
+        data.alwaysOn = !data.alwaysOn;
+        $('.corpo').data('maphilight', data).trigger('alwaysOn.maphilight');
+    })
 }
