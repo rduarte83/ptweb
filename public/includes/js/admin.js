2b5a9d39fdfbaed2e8a9c50fcc203cf45ce6f0f2
João Novo <jnovo@ua.pt>
2018-06-07 19:51:03 +0100

    Admin.js
    - Finally i guess?

--- public/includes/js/admin.js
+++ public/includes/js/admin.js
@@ -40,15 +40,15 @@ function getUsers(){
             var listFunc = '<li class="list-group-item"> ID | Nome</li>';
             var listAdmin = '<li class="list-group-item"> ID | Nome</li>';
             $.each(resposta, function (key, val) {
-                if (resposta[key].role == "3") {
+                if (resposta[key].role == "4") {
                     listFunc += '<li class="list-group-item"><span class="badge badge-primary" style="margin-right: 8px; padding: 10px; vertical-align: middle;">' +resposta[key].id + '</span>' + resposta[key].nome + '</li>';
-                } else if (resposta[key].role == "2") {
+                } else if (resposta[key].role == "2" || resposta[key].role == "3") {
                     listFunc+='<li class="list-group-item"><span class="badge badge-primary" style="margin-right: 8px; padding: 10px; vertical-align: middle;">' + resposta[key].id + '</span>' + resposta[key].nome +'</li>';
                 } else if (resposta[key].role == "1") {
                     listAdmin+='<li id="list_' + resposta[key].id +'" class="list-group-item list-group-item-action" data-toggle="modal" data-target="#addUserModal"><span class="badge badge-primary" style="margin-right: 8px; padding: 10px; vertical-align: middle;">' +resposta[key].id + '</span>' + resposta[key].nome +'</li>';
                 }
             });
-            
+
             $("#list_pacientes").html(listPaciente);
             $("#list_func").html(listFunc);
             $("#list_admin").html(listAdmin);
